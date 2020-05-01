f4722ea091ab01695658c67452cf3bf4d09c0341
David Bond <David.CJ.Bond@gmail.com>
2020-05-01 12:21:44 +1200

    copyright update

--- magicshop.action.php
+++ magicshop.action.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * magicshop implementation : © <Your name here> <Your email address here>
+ * magicshop implementation : © David Bond <David.CJ.Bond@gmail.com>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
